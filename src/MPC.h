bd6332683b6456a6bf619261ae8fbe93789e2d3e
abang <shenzb12@lzu.edu.cn>
2017-10-09 22:47:08 +0800

    finish the project

--- src/MPC.h
+++ src/MPC.h
@@ -7,14 +7,18 @@
 using namespace std;
 
 class MPC {
- public:
-  MPC();
+public:
+    vector<double> mpc_x;
+    vector<double> mpc_y;
 
-  virtual ~MPC();
+    MPC();
 
-  // Solve the model given an initial state and polynomial coefficients.
-  // Return the first actuatotions.
-  vector<double> Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
+    virtual ~MPC();
+
+    // Solve the model given an initial state and polynomial coefficients.
+    // Return the first actuatotions.
+    vector<double> Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
+
 };
 
 #endif /* MPC_H */
